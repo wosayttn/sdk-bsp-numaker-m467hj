667c9fc921641d79e8cecaa8cc614d987eb8d301
Wayne Lin <wclin@nuvoton.com>
2023-08-30 13:42:14 +0800

    Sync with firmware.

--- TestSuite/AutoSnmpTest.bat
+++ TestSuite/AutoSnmpTest.bat
@@ -4,9 +4,9 @@ SnmpGet.exe /?
 
 SnmpGet.exe -r:%ipaddr% -v:2c -c:"public" -o:.1.3.6.1.2.1.1.1.0
 
-SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:md5des -ap:MD5 -aw:nvt1234567 -pp:DES -pw:nvt1234567
-SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:md5aes -ap:MD5 -aw:nvt1234567 -pp:AES128 -pw:nvt1234567
-SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:shades -ap:SHA -aw:nvt1234567 -pp:DES -pw:nvt1234567
-SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:shaaes -ap:SHA -aw:nvt1234567 -pp:AES128 -pw:nvt1234567
+SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:md5des -ap:MD5 -aw:nuvoton_no1 -pp:DES -pw:nuvoton_no1
+SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:md5aes -ap:MD5 -aw:nuvoton_no1 -pp:AES128 -pw:nuvoton_no1
+SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:shades -ap:SHA -aw:nuvoton_no1 -pp:DES -pw:nuvoton_no1
+SnmpGet.exe -r:%ipaddr% -v:3 -o:.1.3.6.1.2.1.1.1.0 -sn:shaaes -ap:SHA -aw:nuvoton_no1 -pp:AES128 -pw:nuvoton_no1
 
 pause
